d5dc8e8ae52cf0cb3b58e5f80e7c8043bfe68d19
Kim Frost <kim@noerd.dk>
2014-10-07 15:19:43 +0200

    (added) - changed age to mass instead, started on growth rate calculations and requirements for water and trimming

--- data/scripts/boxfarmplant.lua
+++ data/scripts/boxfarmplant.lua
@@ -4,19 +4,25 @@ function Create()
     local plantTypes = {
         {
             name = "Cabbage",
+            minGrowthRate = 0,0,
+            maxGrowthRate = 3.0,
             requirements = {
                 nutrients = 700,
                 water = 1200,
                 weeded = true
-            }
+            },
+            subTypeIndexStart = 0;
         },
         {
             name = "Potato",
+            minGrowthRate = 0,0,
+            maxGrowthRate = 2.0,
             requirements = {
                 nutrients = 2200,
                 water = 800,
                 weeded = false
-            }
+            },
+            subTypeIndexStart = 6;
         }
     }
     local numOfPlantTypes = 0;
@@ -27,25 +33,27 @@ function Create()
     -- + 1 because lua is not zero indexed
     local vegIndex = math.floor(math.random() * numOfPlantTypes) + 1;
     local vegType = plantTypes[vegIndex].name;
+    local requirements = plantTypes[vegIndex].requirements;
+    local minGrowthRate = plantTypes[vegIndex].minGrowthRate;
+    local maxGrowthRate = plantTypes[vegIndex].maxGrowthRate;
 
-    Object.SetProperty("Age", 0.0);
+    Object.SetProperty("Mass", 0.0);
     Object.SetProperty("SubType", 0);
     Object.SetProperty("GrowthRate", 1.0);
     Object.SetProperty("VegType", vegType);
+    Object.SetProperty("Requirements", requirements);
+    Object.SetProperty("MinGrowthRate", minGrowthRate);
+    Object.SetProperty("MaxGrowthRate", maxGrowthRate);
 end
 
 function Update(timePassed)
-
-    -- Set variables for later use
-    local maxAge = 300.0;
-    local numOfStates = 5;
 
     -- Get position and material the plant is on
     local ourX = Object.GetProperty("Pos.x");
     local ourY = Object.GetProperty("Pos.y");
     local material = Object.GetMaterial( ourX, ourY );
-    local vegType = Object.GetProperty("VegType");
 
+    -- Require Dirt
     if material ~= "Dirt" then
         Object.SetProperty("Tooltip", "tooltip_boxfarmplant_wrongmaterial");
         return;
@@ -53,14 +61,38 @@ function Update(timePassed)
     -- clear the tooltip
     --Object.SetProperty("Tooltip", "");
 
-    -- Increase Age
-    local age = Object.GetProperty("Age");
-    age = age + timePassed;
-    Object.SetProperty( "Age", age );
+
+    -- Set variables for later use
+    local maxAge = 300.0;
+    local numOfStates = 5;
+    local mass = Object.GetProperty("Mass");
+    local growthRate = Object.GetProperty("GrowthRate");
+    local vegType = Object.GetProperty("VegType");
+    local requirements = Object.GetProperty("Requirements");
+    local minGrowthRate = Object.GetProperty("MinGrowthRate");
+    local maxGrowthRate = Object.GetProperty("MaxGrowthRate");
+
+
+    -- Calculate growth rate
+
+
+    -- Needs Weeded
+
+    -- Needs Water
+
+    -- Needs Fertilizer
+
+
+
+
+    -- Increae mass
+    mass = mass + (timePassed * growthRate);
+    Object.SetProperty( "Mass", Mass );
+
 
     -- Calculate the state of the plant
     local subType = 0;
-    local procentageGrown = 100 / (maxAge / age);
+    local procentageGrown = 100 / (requirements.nutrients / mass);
     local i = 0;
     while i <= numOfStates do
         local TempProcentage = 100 / numOfStates * i;
@@ -75,13 +107,13 @@ function Update(timePassed)
         -- Check for plant being too old
         -- Reset plant stage & age
         SetState(0);
-        Object.SetProperty("Age", 0.0);
+        Object.SetProperty( "Mass", 0.0 );
     else
         SetState(subType);
     end
 
     -- Set tooltips for debug
-    Object.SetProperty("Tooltip", "Age: " .. tostring(age) .. "\n" .. "subType: " .. tostring(subType) .. "\n" .. "Grown: " .. tostring(procentageGrown) .. "%" .. "\n" .. "vegType: " .. tostring(vegType));
+    Object.SetProperty("Tooltip", "Mass: " .. tostring(mass) .. "\n" .. "subType: " .. tostring(subType) .. "\n" .. "Grown: " .. tostring(procentageGrown) .. "%" .. "\n" .. "vegType: " .. tostring(vegType));
 end
 
 
